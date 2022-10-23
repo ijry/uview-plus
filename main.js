d0f49dcd2d7c53b3ffcd077a214de28e0c79c846
jry <ijry@qq.com>
2022-10-23 13:05:15 +0800

    演示增加多语言国际化支持

--- main.js
+++ main.js
@@ -6,6 +6,19 @@ import store from './store'
 // 引入全局uview-plus
 import uviewPlus from './uni_modules/uview-plus/index.js'
 
+// i18n部分的配置
+// 引入语言包，注意路径
+import zhHans from '@/common/locales/zh.js';
+import en from '@/common/locales/en.js';
+const messages = {
+	en,
+	'zh-Hans': zhHans,
+}
+let i18nConfig = {
+  locale: uni.getLocale(),// 获取已设置的语言
+  messages
+}
+
 //import mixin from './common/mixin'
 
 // #ifdef MP
@@ -17,6 +30,8 @@ import uviewPlus from './uni_modules/uview-plus/index.js'
   
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+const i18n = createI18n(i18nConfig)
 import {initRequest} from './util/request/index'
 export function createApp() {
   const app = createSSRApp(App)
@@ -25,6 +40,7 @@ export function createApp() {
   initRequest(app)
 
   app.use(store)
+	.use(i18n)
 	.use(uviewPlus)
   return {
     app
