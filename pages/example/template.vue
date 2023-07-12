44f592441f6f69db980cfa9854922c75e7abfb13
jry <ijry@qq.com>
2023-07-12 15:05:16 +0800

    保存图标

--- pages/example/template.vue
+++ pages/example/template.vue
@@ -32,6 +32,7 @@
 		computed: {
 			getIcon() {
 				return path => {
+					// return './static/uview/demo/' + path + '.png';
 					return 'https://cdn.uviewui.com/uview/example/' + path + '.png';
 				}
 			},
