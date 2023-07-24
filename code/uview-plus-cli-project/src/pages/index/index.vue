5a7f5cf84647ce87aeee6b32e21c18cadcd30ef3
jry <ijry@qq.com>
2023-07-24 21:08:30 +0800

    easycom规则优化

--- code/uview-plus-cli-project/src/pages/index/index.vue
+++ code/uview-plus-cli-project/src/pages/index/index.vue
@@ -5,20 +5,21 @@
       <text class="title">{{ title }}</text>
     </view>
     <view style="padding: 20px;">
-      <u-button type="primary" text="确定"></u-button>
-      <u-button type="success" :plain="true" text="镂空"></u-button>
-      <u-button type="error" :plain="true" :hairline="true" text="细边"></u-button>
-      <u-button type="info" :disabled="disabled" text="禁用"></u-button>
-      <u-button type="warning" loading loadingText="加载中"></u-button>
-      <u-button type="primary" icon="map" text="图标按钮"></u-button>
-      <u-button type="primary" shape="circle" text="按钮形状"></u-button>
-      <u-button text="渐变色按钮" color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))"></u-button>
-      <u-button type="primary" size="small" text="大小尺寸" @click="openDt"></u-button>
-      <u-datetime-picker
+	  <up-input @change="change"></up-input>
+      <up-button type="primary" text="确定"></up-button>
+      <up-button type="success" :plain="true" text="镂空"></up-button>
+      <up-button type="error" :plain="true" :hairline="true" text="细边"></up-button>
+      <up-button type="info" :disabled="disabled" text="禁用"></up-button>
+      <up-button type="warning" loading loadingText="加载中"></up-button>
+      <up-button type="primary" icon="map" text="图标按钮"></up-button>
+      <up-button type="primary" shape="circle" text="按钮形状"></up-button>
+      <up-button text="渐变色按钮" color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))"></up-button>
+      <up-button type="primary" size="small" text="大小尺寸" @click="openDt"></up-button>
+      <up-datetime-picker
         :show="show"
         v-model="value1"
         mode="datetime"
-      ></u-datetime-picker>
+      ></up-datetime-picker>
     </view>
   </view>
 </template>
@@ -33,6 +34,13 @@ const value1 = ref(Number(new Date()))
 
 const openDt = function() {
   show.value = true
+}
+
+const change = function(e) {
+	console.log(e)
+	uni.showToast({
+		title: 'change'
+	})
 }
 
 </script>
