1c052c71e21d2f9f56ed6b48f92d359917e0618f
jry <ijry@qq.com>
2024-03-27 17:20:44 +0800

    增加rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import { visualizer } from "rollup-plugin-visualizer";
 // import commonjs from '@rollup/plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     // commonjs(),
-    uni()
+    uni(),
+	visualizer()
   ],
 });
