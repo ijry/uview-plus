2c244c2ea25fc8a6239701d0a7af1b1db8e505b9
jry <ijry@qq.com>
2024-04-11 11:18:46 +0800

    link change

--- src/pages/componentsD/qrcode/qrcode.vue
+++ src/pages/componentsD/qrcode/qrcode.vue
@@ -3,19 +3,19 @@
 		<view class="u-page__item">
 			<text class="u-page__item__title" style="margin-top: 0;">不带logo</text>
 			<view class="u-page__item__content">
-				<up-qrcode :cid="'up1'" :size="200" val="uview-plus"></up-qrcode>
+				<up-qrcode :cid="'up1'" :size="200" val="https://click.meituan.com/t?t=1&c=2&p=WhaD2b5zGU-h"></up-qrcode>
 			</view>
 		</view>
 		<view class="u-page__item">
 			<text class="u-page__item__title" style="margin-top: 0;">带logo</text>
 			<view class="u-page__item__content">
-				<up-qrcode :cid="'up2'" :size="200" val="uview-plus" :icon="logo"></up-qrcode>
+				<up-qrcode :cid="'up2'" :size="200" val="https://click.meituan.com/t?t=1&c=2&p=WhaD2b5zGU-h" :icon="logo"></up-qrcode>
 			</view>
 		</view>
 		<view class="u-page__item">
 			<text class="u-page__item__title" style="margin-top: 0;">二维码颜色</text>
 			<view class="u-page__item__content">
-				<up-qrcode :cid="'up3'" :size="200" val="uview-plus" background="red" foreground="blue"></up-qrcode>
+				<up-qrcode :cid="'up3'" :size="200" val="https://click.meituan.com/t?t=1&c=2&p=WhaD2b5zGU-h" background="red" foreground="blue"></up-qrcode>
 			</view>
 		</view>
 	</view>
