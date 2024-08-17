cb53ca2d4efb3352b1772a18255231ad6a600982
jry <ijry@qq.com>
2024-08-17 11:24:33 +0800

    add: 通用mixin新增navTo页面跳转方法

--- src/uni_modules/uview-plus/libs/mixin/mixin.js
+++ src/uni_modules/uview-plus/libs/mixin/mixin.js
@@ -106,6 +106,9 @@ export const mixin = defineMixin({
                 // })
             }
         },
+        navTo(url = '', linkType = 'navigateTo') {
+            route({ type: this.linkType, url })
+        },
         // 查询节点信息
         // 目前此方法在支付宝小程序中无法获取组件跟接点的尺寸，为支付宝的bug(2020-07-21)
         // 解决办法为在组件根部再套一个没有任何作用的view元素
