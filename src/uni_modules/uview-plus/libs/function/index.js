dfc1db66ec0616cbfa4a58a98df0b21a6ff4f7a5
宗安 <22-zongan@users.noreply.git.yngishere.com>
2024-10-17 16:44:12 +0800

    修复因为修改组件名称前缀，导致h5打包后$parent方法内找不到父组件的问题

--- src/uni_modules/uview-plus/libs/function/index.js
+++ src/uni_modules/uview-plus/libs/function/index.js
@@ -119,6 +119,7 @@ export function $parent(name = undefined) {
 	// 通过while历遍，这里主要是为了H5需要多层解析的问题
 	while (parent) {
 		// 父组件
+        name = name.replace(/up-([a-zA-Z0-9-_]+)/g, 'u-$1')        
 		if (parent.$options && parent.$options.name !== name) {
 			// 如果组件的name不相等，继续上一级寻找
 			parent = parent.$parent
