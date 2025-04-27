d877fbf63a37eefd6e3f134e2a758d8aa7eb51a4
semdy <501859275@qq.com>
2025-04-27 15:26:28 +0800

    chore: u-picker组件的confirmColor默认值改为空，以便默认使用主题色

--- src/uni_modules/uview-plus/components/u-picker/picker.js
+++ src/uni_modules/uview-plus/components/u-picker/picker.js
@@ -20,7 +20,7 @@ export default {
         cancelText: '取消',
         confirmText: '确定',
         cancelColor: '#909193',
-        confirmColor: '#3c9cff',
+        confirmColor: '',
         visibleItemCount: 5,
         keyName: 'text',
         closeOnClickOverlay: false,
