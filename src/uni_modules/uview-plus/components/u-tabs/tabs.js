7c1693d300992d39953c225057758c6a19bc3d90
jry <ijry@qq.com>
2025-08-07 16:53:46 +0800

    feat: 取消props中tabs组件默认颜色影响主题色生效

--- src/uni_modules/uview-plus/components/u-tabs/tabs.js
+++ src/uni_modules/uview-plus/components/u-tabs/tabs.js
@@ -12,7 +12,7 @@ export default {
     tabs: {
         duration: 300,
         list: [],
-        lineColor: '#3c9cff',
+        lineColor: '',
         activeStyle: {
             color: '#303133'
         },
