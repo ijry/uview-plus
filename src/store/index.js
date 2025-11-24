63ec4d683932a86b4d6cc24a396deae5f9df3d63
jry <ijry@qq.com>
2025-11-24 18:26:56 +0800

    鸿蒙打包配置

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
 // https://uniapp.dcloud.net.cn/tutorial/vue3-pinia.html#%E7%8A%B6%E6%80%81%E7%AE%A1%E7%90%86-pinia
 import * as Pinia from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+// import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const pinia = Pinia.createPinia()
-pinia.use(piniaPluginPersistedstate)
+// pinia.use(piniaPluginPersistedstate)
 
 export default pinia
